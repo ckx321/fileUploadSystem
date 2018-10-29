c34a0ed898fa02ffbbe702d51186ff10d67928b3
ckx <303813367@qq.com>
2018-10-29 09:52:05 +0800

    文件缴存系统

---
+++ 资料/需求文档/20181026/文件缴存系统10-16日修改/files/系统设置/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t),u,_(),v,_(w,x,y,z,A,B,C,_(),D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,H,P,H,Q,R,S,null,T,U,V,W,X,Y,Z,U),ba,_(),bb,_(),bc,_(bd,[])),be,_(),bf,_());}; 
+var b="url",c="系统设置.html",d="generationDate",e=new Date(1540173409070.52),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="defaultBackgroundImageInfo",o="path",p="images/___data_editor__/bg.png",q="width",r=2230,s="height",t=1212,u="viewIdToBackgroundImageInfo",v="page",w="packageId",x="c10114150dc04f8f8099044a8a26cf94",y="type",z="Axure:Page",A="name",B="系统设置",C="notes",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="near",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageHorizontalAlignment",P="imageVerticalAlignment",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="adaptiveStyles",bb="interactionMap",bc="diagram",bd="objects",be="masters",bf="objectPaths";
+return _creator();
+})());
