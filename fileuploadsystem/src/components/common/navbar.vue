97373141b526e82c413d9b33aaccd6911c98eec9
ckx <303813367@qq.com>
2018-11-06 14:55:11 +0800

    增加了后台项目

--- fileuploadsystem/src/components/common/navbar.vue
+++ fileuploadsystem/src/components/common/navbar.vue
@@ -8,7 +8,7 @@
       </el-col>
       <el-col :span="17">
         <el-menu mode="horizontal">
-          <el-menu-item index="1" class="text_blue">资源计划</el-menu-item>
+          <el-menu-item index="1" class="text_blue">资源 计划</el-menu-item>
           <el-menu-item index="2">协同编纂</el-menu-item>
           <el-menu-item index="3">资源管理</el-menu-item>
           <el-menu-item index="4">运营管理</el-menu-item>
